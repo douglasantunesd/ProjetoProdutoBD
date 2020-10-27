fdca4093fea4bcf84b363bebca1749cfd0a15832
douglasantunesd <70909360+douglasantunesd@users.noreply.github.com>
2020-10-26 21:09:48 -0300

    Update CadastroProdutoActivity.java

--- app/src/main/java/com/example/projeto01/CadastroProdutoActivity.java
+++ app/src/main/java/com/example/projeto01/CadastroProdutoActivity.java
@@ -7,15 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.projeto01.database.ProdutoDAO;
 import com.example.projeto01.modelo.Produto;
 
 public class CadastroProdutoActivity extends AppCompatActivity {
 
-    private final int RESULT_CODE_NOVO_PRODUTO = 10;
-    private final int RESULT_CODE_PRODUTO_EDITADO = 11;
-
-    private boolean edicao = false;
     private int id = 0;
 
     @Override
@@ -26,46 +24,38 @@ public class CadastroProdutoActivity extends AppCompatActivity {
         carregarProduto();
     }
 
-    private void carregarProduto(){
+    public  void carregarProduto(){
         Intent intent = getIntent();
-        if (intent != null && intent.getExtras() != null &&
-                intent.getExtras().get("produtoEdicao") != null) {
+        if ( intent != null && intent.getExtras() != null && intent.getExtras().get("produtoEdicao") != null){
             Produto produto = (Produto) intent.getExtras().get("produtoEdicao");
             EditText editTextNome = findViewById(R.id.editTextT_nome);
             EditText editTextValor = findViewById(R.id.editText_valor);
             editTextNome.setText(produto.getNome());
             editTextValor.setText(String.valueOf(produto.getValor()));
-            edicao = true;
             id = produto.getId();
+
         }
     }
-
 
 
     public void onClickVoltar(View v){
         finish();
     }
 
-    public void onClickSalvar(View v){
+    public void onClickSalvar(View v) {
         EditText editTextNome = findViewById(R.id.editTextT_nome);
-        EditText editTextvalor = findViewById(R.id.editText_valor);
+        EditText editTextValor = findViewById(R.id.editText_valor);
 
         String nome = editTextNome.getText().toString();
-        Float valor = Float.parseFloat(editTextvalor.getText().toString());
+        Float valor = Float.parseFloat(editTextValor.getText().toString());
 
-        Produto produto = new Produto(id, nome, valor);
-        Intent intent = new Intent();
-          if (edicao){
-            intent.putExtra("produtoEditado", produto);
-            setResult(RESULT_CODE_PRODUTO_EDITADO, intent);
+        Produto produto = new Produto(id,nome, valor);
+        ProdutoDAO produtoDAO = new ProdutoDAO(getBaseContext());
+        boolean salvou = produtoDAO.salvar(produto);
+        if (salvou){
+            finish();
         } else {
-            intent.putExtra("novoProduto", produto);
-            setResult(RESULT_CODE_NOVO_PRODUTO, intent);
-
+            Toast.makeText(CadastroProdutoActivity.this, "ERRO ao Salvar", Toast.LENGTH_LONG).show();
         }
-
-        finish();
-
     }
-
 }
